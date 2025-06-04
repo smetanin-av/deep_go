55ea5b34956348eb825b2d327be71efdb7562708
Alexey Smetanin <smetanin-alexey@mail.ru>
2025-06-04 10:00:24 +0300

    feat: implement MultiError (#8)
    Co-authored-by: Aleksei Smetanin <smetanin.aleksey12@rwb.ru>

--- homework/errors/homework_test.go
+++ homework/errors/homework_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"errors"
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -10,17 +12,50 @@ import (
 // go test -v homework_test.go
 
 type MultiError struct {
-	// need to implement
+	errs []error
 }
 
 func (e *MultiError) Error() string {
-	// need to implement
-	return ""
+	switch len(e.errs) {
+	case 0:
+		return ""
+	case 1:
+		return e.errs[0].Error()
+	}
+
+	sb := strings.Builder{}
+	sb.WriteString(fmt.Sprintf("%d errors occured:\n", len(e.errs)))
+
+	for _, err := range e.errs {
+		sb.WriteString(fmt.Sprintf("\t* %v", err))
+	}
+
+	sb.WriteByte('\n')
+	return sb.String()
+}
+
+func (e *MultiError) Append(errs ...error) {
+	for _, v := range errs {
+		if v == nil {
+			continue
+		}
+		if m, ok := v.(*MultiError); ok {
+			e.errs = append(e.errs, m.errs...)
+		} else {
+			e.errs = append(e.errs, v)
+		}
+	}
+}
+
+func (e *MultiError) Unwrap() []error {
+	return e.errs
 }
 
 func Append(err error, errs ...error) *MultiError {
-	// need to implement
-	return nil
+	var res MultiError
+	res.Append(err)
+	res.Append(errs...)
+	return &res
 }
 
 func TestMultiError(t *testing.T) {
@@ -30,4 +65,12 @@ func TestMultiError(t *testing.T) {
 
 	expectedMessage := "2 errors occured:\n\t* error 1\t* error 2\n"
 	assert.EqualError(t, err, expectedMessage)
+
+	err3 := errors.New("error 3")
+	err = Append(err, err3)
+	assert.ErrorIs(t, err, err3)
+
+	var merr *MultiError
+	assert.ErrorAs(t, err, &merr)
+	assert.Contains(t, merr.errs, err3)
 }
